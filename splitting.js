aca81869e93c9c8ab2211bf64f5aedcd1a942ece
Alex Bogdan <stefanalexb@gmail.com>
2019-02-03 07:12:36 -0500

    splitting eta

--- splitting.js
+++ splitting.js
@@ -11,3 +11,25 @@ function selectPerson(btn){
         count=0;
     }
 }
+
+
+function populate(){
+	var transform = {"tag":"table", "children":[
+    {"tag":"tbody","children":[
+        {"tag":"tr","children":[
+            {"tag":"td","html":"${name}"},
+            {"tag":"td","html":"${age}"}
+        ]}
+    ]}
+]};
+
+var data = [
+    {'name':'Bob','age':40},
+    {'name':'Frank','age':15},
+    {'name':'Bill','age':65},
+    {'name':'Robert','age':24}
+];
+
+$('#target_div').html(json2html.transform(data,transform));
+	
+}
